ed203a09f7819e75ac75615d13ae9ece0a110af8
Andrew <akolisnyk@malkosua.com>
2017-08-10 14:48:29 +0300

    change url string to modules

--- modules/InitalLoad/Privat24.js
+++ modules/InitalLoad/Privat24.js
@@ -1,8 +1,8 @@
 const request = require('request');
-const Promise = require('promise');
+const URLS = require('../../Types/Links');
 
 module.exports = function (req, res) {
-    request('https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014', (err, response, data) => {
+    request(`${URLS.uah}01.12.2014`, (err, response, data) => {
         if (response.statusCode === 200) {
             res.send(data)
         }
